341619a3edccbbce0c062be4168b73e5f4611105
Ibrahim <ibrahem7884026@gmail.com>
2023-05-30 01:04:15 +0300

    output prime number base 2 variable

--- prime number.py
+++ prime number.py
@@ -1,14 +1,16 @@
 
+# Python program to display all the prime numbers within an interval
 
-num1 = int(input("Enter the lower Interval number : "))
-num2 = int(input("Enter the upper Interval number: "))
+lower = 900
+upper = 1000
 
-print("Prime numbers from ", num1 ," to ", num2 ," are: ")
+print("Prime numbers between", lower, "and", upper, "are:")
 
-for num in range(num1, num2 + 1):
-    if num > 1:
-        for i in range(2, num):
-            if (num % i == 0):
-                break
-        else:
-            print(num)
+for num in range(lower, upper + 1):
+   # all prime numbers are greater than 1
+   if num > 1:
+       for i in range(2, num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
